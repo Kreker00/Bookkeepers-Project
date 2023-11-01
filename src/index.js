a513210454e4cfc1b5b52c9395421f0996f945c0
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-11-01 20:36:26 +0200

    Initialize shopping-list

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/basket/basket';
+import './js/books/books';
+import './js/category/category';
+import './js/header/header';
+import './js/modal/modal';
+import './js/support-ukraine/support-ukraine';
