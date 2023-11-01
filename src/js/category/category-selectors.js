61455c0da192167798ff76cff71f8ba28b3a558b
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-11-01 20:36:25 +0200

    Initialize header-dark

---
+++ src/js/category/category-selectors.js
@@ -0,0 +1,6 @@
+export const categorySelectors = {
+    categoryContainer: document.querySelector('.category-container'),
+    categoryList: document.querySelector('.category-list'),
+    categoryItem: document.querySelector('.category-item'),
+activeCategory: document.querySelector('.category-active')
+}
